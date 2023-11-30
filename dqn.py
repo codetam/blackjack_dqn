537fa704b167f092fe92d6a6178b233dd64d02ba
codetam <tam@DESKTOP-FNN0JLC>
2023-11-30 18:30:17 +0100

    Added prioritized experience buffer

--- dqn.py
+++ dqn.py
@@ -17,8 +17,8 @@ MINIBATCH_SIZE = 128
 MODEL_NAME="128x64"
 DISCOUNT = 0.99
 UPDATE_TARGET_EVERY = 5
-LEARNING_RATE = 0.01
-LOAD_MODEL = "models/128x64_____0.18avg__1701277765.model"
+LEARNING_RATE = 0.001
+LOAD_MODEL = "models/128x64_____0.12avg__1701320928.model"
 
 # Own Tensorboard class
 class ModifiedTensorBoard(TensorBoard):
@@ -54,9 +54,44 @@ class ModifiedTensorBoard(TensorBoard):
                 tf.summary.scalar(key, value, step = self.step)
                 self.writer.flush()
 
+class ReplayBuffer():
+    def __init__(self, maxlen):
+        self.buffer = deque(maxlen=maxlen)
+        self.priorities = deque(maxlen=maxlen)
+
+    def add(self, experience):
+        self.buffer.append(experience)
+        self.priorities.append(max(self.priorities, default=1))
+
+    def get_probabilities(self, alpha):
+        scaled_priorities = np.array(self.priorities) ** alpha
+        sample_probabilities = scaled_priorities / sum(scaled_priorities)
+        return sample_probabilities
+    
+    def len(self):
+        return len(self.buffer)
+
+    def get_importance(self, probabilities):
+        importance = 1/len(self.buffer) * 1/probabilities
+        importance_normalized = importance / max(importance)
+        return importance_normalized
+
+    def sample(self, batch_size, alpha=1.0):
+        sample_probs = self.get_probabilities(alpha)
+        sample_indices = random.choices(range(len(self.buffer)), k=batch_size, weights=sample_probs)
+        samples = [self.buffer[pos] for pos in sample_indices]
+        importance = self.get_importance(sample_probs[sample_indices])
+        return samples, importance, sample_indices
+    
+    def set_priorities(self, indices, errors, offset=0.1):
+        for i, e in zip(indices, errors):
+            self.priorities[i] = abs(e[0]) + abs(e[1]) + offset
+
+
 class DQNAgent:
     def __init__(self, env):
         self.env = env
+        self.importance_in = 1
         # main model, this is trained every step
         self.model = self.create_model()
 
@@ -64,7 +99,12 @@ class DQNAgent:
         self.target_model = self.create_model()
         self.target_model.set_weights(self.model.get_weights())
 
-        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+        # epsilon
+        self.epsilon = 1
+        self.eps_decay = 0.99
+        self.eps_min = 0.1
+        
+        self.replay_buffer = ReplayBuffer(maxlen=10000)
         self.tensorboard = ModifiedTensorBoard(log_dir=f"logs/{MODEL_NAME}-{int(time.time())}")
         self.target_update_counter = 0 # track when we're ready to update the target model
 
@@ -76,26 +116,26 @@ class DQNAgent:
             print(f"Loading {LOAD_MODEL}")
             model = load_model(LOAD_MODEL)
             print(f"Model {LOAD_MODEL} loaded")
+            model.compile(loss=self.weighted_mse_loss, optimizer=Adam(learning_rate=LEARNING_RATE), metrics=['accuracy'], run_eagerly=True)
         else:
             model = Sequential()
             model.add(Dense(128, activation="relu", input_shape=(OBS_SPACE_SIZE,)))
             model.add(Dense(64, activation="relu"))
             model.add(Dense(self.env.action_space.n, activation="linear"))
-            model.compile(loss="mse", optimizer=Adam(learning_rate=LEARNING_RATE), metrics=['accuracy'])
+            model.compile(loss=self.weighted_mse_loss, optimizer=Adam(learning_rate=LEARNING_RATE), metrics=['accuracy'], run_eagerly=True)
         return model
     
-    def update_replay_memory(self, transition):
-        self.replay_memory.append(transition)
+    def weighted_mse_loss(self, y_true, y_pred):
+        importance_float32 = tf.cast(self.importance_in, dtype=tf.float32)
+        importance_reshaped = tf.reshape(importance_float32, [-1, 1])
+        td_error = y_true - y_pred
+        self.losses = tf.multiply(tf.square(td_error), importance_reshaped)
+        return tf.reduce_mean(self.losses)
 
     def get_qs(self, state):
         return self.model.predict(np.reshape(state, (1, OBS_SPACE_SIZE)), verbose=0)[0]
     
-    def train(self, terminal_state):
-        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
-            return
-        
-        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
-
+    def create_train_dataset(self, minibatch):
         current_states = np.reshape([transition[0] for transition in minibatch], (MINIBATCH_SIZE, OBS_SPACE_SIZE))
         current_qs_list = self.model.predict(current_states, verbose=0)
 
@@ -104,7 +144,6 @@ class DQNAgent:
 
         X = []
         y = []
-
         for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
             if not done:
                 max_future_q = np.max(future_qs_list[index])
@@ -117,14 +156,36 @@ class DQNAgent:
 
             X.append(np.reshape(current_state, (OBS_SPACE_SIZE, )))
             y.append(current_qs)
+        return X, y
 
-        # we only fit if we are in our terminal state
-        self.model.fit(np.array(X), np.array(y), batch_size = MINIBATCH_SIZE, 
-                       verbose=0, shuffle=False, callbacks=[self.tensorboard] if terminal_state else None)
+    def train(self, current_state, action, reward, new_state, done):
+        self.replay_buffer.add((current_state, action, reward, new_state, done))
+        if self.replay_buffer.len() < MIN_REPLAY_MEMORY_SIZE:
+            return
         
-        if terminal_state:
+        minibatch, importance, indices = self.replay_buffer.sample(MINIBATCH_SIZE)
+        self.importance_in = importance**(1-self.epsilon)
+        X, y = self.create_train_dataset(minibatch)
+
+        self.model.fit(np.array(X), np.array(y), batch_size = MINIBATCH_SIZE, 
+                       verbose=0, shuffle=False, callbacks=[self.tensorboard] if done else None)
+        
+        self.replay_buffer.set_priorities(indices, self.losses)
+        if done:
             self.target_update_counter += 1
+            # Decay epsilon
+            self.epsilon = max(self.eps_min, self.epsilon * self.eps_decay)
         
         if self.target_update_counter > UPDATE_TARGET_EVERY:
             self.target_model.set_weights(self.model.get_weights())
             self.target_update_counter = 0
+
+
+    def get_action(self, current_state):
+        if np.random.random() > self.epsilon:
+            # Get action from Q table
+            action = np.argmax(self.get_qs(current_state))
+        else:
+            # Get random action
+            action = np.random.randint(0, self.env.action_space.n)
+        return action
--- test.py
+++ test.py
@@ -1,27 +1,21 @@
 from blackjack import BlackjackEnv
 from dqn import DQNAgent
-import numpy as np
 from tqdm import tqdm
 import time
 import os
 import tensorflow as tf
-from collections import deque
 
 if not os.path.isdir('models'):
     os.makedirs('models')
 
 TRAIN = True
 
-# gpus = tf.config.list_physical_devices('GPU')
-# tf.config.experimental.set_memory_growth(gpus[0], True)
-# logical_gpus = tf.config.list_logical_devices('GPU')
-# print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+gpus = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+logical_gpus = tf.config.list_logical_devices('GPU')
+print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
 
 EPISODES = 50_000
-# Exploration settings
-epsilon = 1  # not a constant, going to be decayed
-EPSILON_DECAY = 0.99
-MIN_EPSILON = 0.01
 
 #  Stats settings
 AGGREGATE_STATS_EVERY = 50  # episodes
@@ -45,22 +39,15 @@ if TRAIN:
         current_state = env.reset()
         done = False
         while not done:
-            if np.random.random() > epsilon:
-                # Get action from Q table
-                action = np.argmax(agent.get_qs(current_state))
-            else:
-                # Get random action
-                action = np.random.randint(0, env.action_space.n)
-
+            action = agent.get_action(current_state)
             new_state, reward, done, _ = env.step(action)
+
             # Transform new continous state to new discrete state and count reward
             episode_reward += reward
             if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
                 env.render()
 
-            agent.update_replay_memory((current_state, action, reward, new_state, done))
-            agent.train(done)
-
+            agent.train(current_state, action, reward, new_state, done)
             current_state = new_state
         
         # Append episode reward to a list and log stats (every given number of episodes)
@@ -69,13 +56,10 @@ if TRAIN:
             average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
             min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
             max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
+            agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=agent.epsilon)
 
             # Save model, but only when min reward is greater or equal a set value
             if episode % 5000 == 0:
                 agent.model.save(f'models/128x64__{average_reward:_>7.2f}avg__{int(time.time())}.model')
 
-        # Decay epsilon
-        if epsilon > MIN_EPSILON:
-            epsilon *= EPSILON_DECAY
-            epsilon = max(MIN_EPSILON, epsilon)
+        tf.keras.backend.clear_session()
