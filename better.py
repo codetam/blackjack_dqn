7d30fcad1e7dbccbb24930219ba9a049cd702548
codetam <tammaro02cimmino@gmail.com>
2023-12-08 19:45:24 +0100

    Added betting agent

--- better.py
+++ better.py
@@ -1,25 +1,44 @@
 import numpy as np
-from collections import deque
-from blackjack import calculate_sum, get_card_num
+from blackjack import calculate_sum, check_soft_hand
 
 class BettingAgent():
-    def __init__(self, env, playerAgent, stake):
-        self.env = env
-        self.playerAgent = playerAgent
-        self.stake = stake
+    def __init__(self, env, dqnAgent, max_bet):
+        self.num_decks = env.blackjackEnv.num_decks
+        self.dqnAgent = dqnAgent
+        self.max_bet = max_bet
+        self.state_buffer = []
+        self.combinations = []
 
+    # Increase the element of the state by the amount returned
     def get_value_to_add(self, next_state, i):
+        # Card value won't be considered if there are no more left
         if next_state[i] == 1:
             value_to_add = 0
         else:
-            value_to_add = 1 / (4 * self.env.num_decks)
+            value_to_add = 1 / (4 * self.num_decks)
             if i == 9:
-                # There are more 10's in the deck
-                value_to_add = 1 / (16 * self.env.num_decks)
-
+                value_to_add = 1 / (16 * self.num_decks)
         return value_to_add
     
-    def update_state_buffer(self, iter_number, state, player_cards):
+    # Finds the number of possible ways the selected cards can be drawn
+    def get_num_combinations(self, state, cards):
+        state_denormalized = np.copy(state)
+        # Finds the amount of cards drawn
+        state_denormalized[0:9] = state_denormalized[0:9] * 4 * self.num_decks
+        state_denormalized[9] = state_denormalized[9] * 16 * self.num_decks
+        num_combinations = 1
+        for card in cards:
+            state_denormalized[card - 1] = state_denormalized[card - 1] - 1
+            if card == 10:
+                total = 16 * self.num_decks
+            else:
+                total = 4 * self.num_decks
+            # the number of combination
+            num_combinations *= (total - state_denormalized[card - 1])
+        return num_combinations
+    
+    # Recursive function to fill the state and the combinations buffers
+    def update_buffers(self, iter_number, state, player_cards):
         for i in range(10):
             next_state = np.copy(state)
             value_to_add = self.get_value_to_add(next_state, i)
@@ -29,26 +48,44 @@ class BettingAgent():
             if iter_number == 0:
                 # Adds the card to the dealer's hand
                 next_state[11] = (i + 1) / 10
+                self.update_buffers(iter_number + 1, next_state, player_cards)
             elif iter_number == 1:
+                # Adds the card to the player's hand
                 player_cards = np.array([0, 0])
                 player_cards[0] = i + 1
+                self.update_buffers(iter_number + 1, next_state, player_cards)
             elif iter_number == 2:
                 player_cards = np.copy(player_cards)
+                # Adds the card to the player's hand
                 player_cards[1] = i + 1
                 next_state[10] = calculate_sum(player_cards) / 21
-                next_state[12] = 0
-                player_card_numbers = np.array([get_card_num(card) for card in player_cards])
-                hard_sum = np.sum(player_card_numbers)
-                if 1 in player_card_numbers and (hard_sum + 10) <= 21:
-                    # The current sum is soft
-                    next_state[12] = 1
-                self.state_buffer.append(next_state)
-            if iter_number < 2:
-                self.update_state_buffer(iter_number + 1, next_state, player_cards)
+                next_state[12] = check_soft_hand(player_cards)
 
-    def calculate_bet(self, current_state):
+                cards = [int(next_state[11] * 10), player_cards[0], player_cards[1]]
+                num_combinations = self.get_num_combinations(next_state[:10], cards)
+
+                self.state_buffer.append(next_state)
+                self.combinations.append(num_combinations)
+
+    def fill_buffers(self, current_state):
         state = np.array(current_state)
-        self.state_buffer = deque()
-        self.update_state_buffer(0, state, np.array([0, 0]))
-        return self.state_buffer
-        #qs = self.playerAgent.get_qs(states=all_states, batch_size=1)
+        self.state_buffer = []
+        self.combinations = []
+        self.update_buffers(0, state, np.array([0, 0]))
+    
+    def get_expected_return(self, current_state):
+        self.fill_buffers(current_state)
+        qs_list = self.dqnAgent.get_qs(states=np.array(self.state_buffer), batch_size=len(self.state_buffer))
+        max_qs = [max(qs) for qs in qs_list]
+        expected_return = 0
+        for i in range(len(max_qs)):
+            expected_return += max_qs[i] * self.combinations[i]
+        expected_return /= sum(self.combinations)
+        return expected_return
+    
+    # Returns the bet amount
+    def get_action(self, current_state):
+        expected_return = self.get_expected_return(current_state)
+        if expected_return < 0:
+            expected_return = 0
+        return min(expected_return * self.max_bet, self.max_bet)
