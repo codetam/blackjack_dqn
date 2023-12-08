27c7e9db4e8a8cf4a810136fdcc2b47f54f4b859
codetam <tammaro02cimmino@gmail.com>
2023-12-08 19:43:02 +0100

    Added betting environment

--- blackjack.py
+++ blackjack.py
@@ -1,18 +1,57 @@
-from gamecode import print_gamestate, hit, dealer_turn, turn_init, get_card_num, calculate_sum, print_table
-  
+from gamecode import print_gamestate, hit, dealer_turn, turn_init, get_card_num, calculate_sum, print_table, check_soft_hand
 import gym
 from gym import spaces
-
 import numpy as np
+
+class BlackjackWithBetEnv(gym.Env):
+    metadata = {
+        "render_modes": [],
+    }
+
+    def __init__(self, blackjackEnv, playerAgent, max_bet):
+        self.blackjackEnv = blackjackEnv
+        self.playerAgent = playerAgent
+        self.action_space = spaces.Discrete(max_bet + 1)
+        self.observation_space = spaces.Box(0, 1, shape=(13,))
+    
+    def reset(self, seed=None):
+        observation = self.blackjackEnv.reset(init_hands=False)
+        return observation
+
+    def step(self, action, render_steps=False):
+        assert self.action_space.contains(action)
+        bet = action
+        current_state = self.blackjackEnv.initialize_hands()
+        if render_steps:
+            self.blackjackEnv.render()
+        done = False
+        total_reward = 0
+        while not done:
+            action = self.playerAgent.get_action(current_state)
+            new_state, reward, done, _ = self.blackjackEnv.step(int(action))
+            if render_steps:
+                self.blackjackEnv.render(action, done, reward*bet)
+            current_state = new_state
+            total_reward += reward
+        reward *= bet
+        return current_state, reward, done, {}
+    
+    def render(self, action=-1, done=False, outcome=0):
+        self.blackjackEnv.render(action, done, outcome)
+
+    def get_obs(self):
+        return self.blackjackEnv.get_obs()
+
 
 class BlackjackEnv(gym.Env):
     metadata = {
         "render_modes": [],
     }
 
-    def __init__(self, num_decks):
+    def __init__(self, num_decks=2):
         # 0 -> Stand
         # 1 -> Hit
+        # 2 -> Double down
         self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Box(0, 1, shape=(13,))
         self.num_decks = num_decks
@@ -20,7 +59,7 @@ class BlackjackEnv(gym.Env):
         self.max_turn = 13
 
 
-    def _get_obs(self):
+    def get_obs(self):
         # normalization of cards out
         box_obs = np.empty((13,), dtype=np.float32)
         box_obs[:9] = self.cards_out[:-1] / (4 * self.num_decks)
@@ -30,15 +69,15 @@ class BlackjackEnv(gym.Env):
         box_obs[10] = calculate_sum(self.player_cards) / 21
         box_obs[11] = get_card_num(self.dealer_cards[0]) / 10
 
-        box_obs[12] = 0
-        player_card_numbers = np.array([get_card_num(card) for card in self.player_cards])
-        hard_sum = np.sum(player_card_numbers)
-        if 1 in player_card_numbers and (hard_sum + 10) <= 21:
-            # The current sum is soft
-            box_obs[12] = 1
+        box_obs[12] = check_soft_hand(self.player_cards)
         return (box_obs)
     
-    def reset(self, seed = None):
+    def initialize_hands(self):
+        self.player_cards, self.dealer_cards, self.deck, self.cards_out = turn_init(self.deck, self.cards_out)
+        observation = self.get_obs()
+        return observation
+    
+    def reset(self, seed = None, init_hands=True):
         if self.turn % self.max_turn == 0:
             super().reset(seed=seed)
             self.turn = 1
@@ -50,12 +89,13 @@ class BlackjackEnv(gym.Env):
             self.cards_out = np.zeros((10,), dtype=np.int8)
         else:
             self.turn += 1
+        self.player_cards = np.zeros((10,), dtype=np.int8)
+        self.dealer_cards = np.zeros((10,), dtype=np.int8)
 
-        # Initialize hands
-        player_cards, dealer_cards, self.deck, self.cards_out = turn_init(self.deck, self.cards_out)
-        self.player_cards = player_cards
-        self.dealer_cards = dealer_cards
-        observation = self._get_obs()
+        if init_hands:
+            observation = self.initialize_hands()
+        else:
+            observation = self.get_obs()
         return observation
 
     def step(self, action):
@@ -68,7 +108,6 @@ class BlackjackEnv(gym.Env):
         elif action == 1:
             self.player_cards, self.deck, self.cards_out, result = hit(self.player_cards, self.deck, self.cards_out)
             if result == -1:
-                # player has busted
                 reward = self.agent_has_finished(True, 1)
             elif result == 21:
                 reward = self.agent_has_finished(False, 1)
@@ -79,12 +118,11 @@ class BlackjackEnv(gym.Env):
         else:
             self.player_cards, self.deck, self.cards_out, result = hit(self.player_cards, self.deck, self.cards_out)
             if result == -1:
-                # player has busted
                 reward = self.agent_has_finished(True, 2)
             else:
                 reward = self.agent_has_finished(False, 2)
 
-        observation = self._get_obs()
+        observation = self.get_obs()
         return observation, reward, is_done, {}
         
     def agent_has_finished(self, busted, factor):
