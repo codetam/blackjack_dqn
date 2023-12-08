096195287e23ac035fbc1ffe7004fefbdd31108f
codetam <tammaro02cimmino@gmail.com>
2023-12-08 19:47:44 +0100

    Added test for the betting agent

--- test.py
+++ test.py
@@ -1,39 +1,36 @@
-from blackjack import BlackjackEnv
-from baseline import BaselineAgent
+from blackjack import BlackjackEnv, BlackjackWithBetEnv
+from dqn import DQNAgent
 from better import BettingAgent
-import numpy as np
 
-env = BlackjackEnv(2)
-agent = BaselineAgent()
-# 
-# total_reward = 0
-# 
-# num_turns = 20
-# for i in range(num_turns):
-#     current_state = env.reset()
-#     done = False
-#     env.render()
-#     current_stake = 0
-#     while not done:
-#         action = agent.get_action(current_state)
-#         new_state, reward, done, _ = env.step(int(action))
-#         env.render(action, done, reward)
-#         current_state = new_state
-#         total_reward += reward
-# 
-# print("Average reward: ", total_reward/num_turns)
+blackjack_env = BlackjackEnv(2)
+agent = DQNAgent(env=blackjack_env, epsilon=0, loaded_model="models/128x64_noexp_2")
+betting_env = BlackjackWithBetEnv(blackjackEnv=blackjack_env, playerAgent=agent, max_bet=10_000)
+better = BettingAgent(env=betting_env, dqnAgent=agent, max_bet=100)
 
-better = BettingAgent(env=env, playerAgent=agent, stake=1000)
-state = np.zeros(13)
-state_buffer = better.calculate_bet(state)
-i = 1
-for state in state_buffer:
-    print(str(i) + " [", end="")
-    for value in state[:9]:
-        print(str(int(value * 8)) + " ", end="")
-    print(str(int(state[9] * 32)) + " ", end="")
-    print(str(int(state[10] * 21)) + " ", end="")
-    print(str(int(state[11] * 10)) + " ", end="")
-    print(str(state[12]), end="")
-    print("]\n")
-    i += 1
+total_reward = 0
+num_turns = 13
+if better is not None:
+    for i in range(num_turns):
+        current_state = betting_env.reset()
+        done = False
+        betting_env.render()
+        print("Expected return: ", better.get_expected_return(current_state))
+
+        action = better.get_action(current_state)
+        new_state, reward, done, _ = betting_env.step(int(action), render_steps=True)
+        current_state = new_state
+        total_reward += reward
+else:
+    for i in range(num_turns):
+        current_state = blackjack_env.reset()
+        done = False
+        blackjack_env.render()
+        current_stake = 0
+        while not done:
+            action = agent.get_action(current_state)
+            new_state, reward, done, _ = blackjack_env.step(int(action))
+            blackjack_env.render(action, done, reward)
+            current_state = new_state
+            total_reward += reward
+
+print("Money lost/won: ", total_reward)
