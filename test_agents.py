963409831d300053a01da17c07d65ab767453631
codetam <tammaro02cimmino@gmail.com>
2023-12-18 22:53:57 +0100

    Fixed bugs

--- test_agents.py
+++ test_agents.py
@@ -1,9 +1,6 @@
-from blackjack_envs import BlackjackEnv, BlackjackWithBetEnv
+from blackjack_envs import BlackjackEnv
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-from collections import deque
 
-test_bet = False
 test_baseline = False
 test_dqn = False
 all = True
@@ -66,75 +63,61 @@ env_1 = BlackjackEnv(2)
 env_2 = BlackjackEnv(2)
 env_3 = BlackjackEnv(2)
 
+DQN_model = 'models/128x64x32_val=-0.165_1702903139.model'
+
 num_turns = 1000
-if test_bet:
+if test_baseline:
+    from agents.baseline import BaselineAgent
+    agent = BaselineAgent()
+    test_agent(agent=agent, blackjack_env=env_2, num_turns=num_turns)
+if test_dqn:
     from agents.dqn import DQNAgent
-    from agents.better import BettingAgent
-    agent = DQNAgent(env=env_1, epsilon=0, loaded_model="models/128x64_noexp_2")
-    betting_env = BlackjackWithBetEnv(blackjackEnv=env_1, playerAgent=agent, max_bet=10_000)
-    better = BettingAgent(env=betting_env, dqnAgent=agent, max_bet=100)
+    agent = DQNAgent(env=env_1, epsilon=0, loaded_model=DQN_model)
+    test_agent(agent=agent, blackjack_env=env_2, num_turns=num_turns)
+if all:
+    from agents.baseline import BaselineAgent
+    from agents.dqn import DQNAgent
+    from agents.random_agent import RandomAgent
+    baseline_agent = BaselineAgent()
+    dqn_agent = DQNAgent(env=env_1, epsilon=0, loaded_model=DQN_model)
+    random_agent = RandomAgent()
+    plays = []
+    num_plays = 0
+    baseline_data = {'rewards': [], 'avg_rewards': []}
+    dqn_data = {'rewards': [], 'avg_rewards': []}
+    random_data = {'rewards': [], 'avg_rewards': []}
+    plt.show()
     for i in range(num_turns):
-        current_state = betting_env.reset()
-        done = False
-        betting_env.render()
-        print("Expected return: ", better.get_expected_return(current_state))
+        rew1 = end_play(agent=baseline_agent, env=env_1)
+        rew2 = end_play(agent=dqn_agent, env=env_2)
+        rew3 = end_play(agent=random_agent, env=env_3)
 
-        action = better.get_action(current_state)
-        new_state, reward, done, _ = betting_env.step(int(action), render_steps=True)
-        current_state = new_state
-else:
-    if test_baseline:
-        from agents.baseline import BaselineAgent
-        agent = BaselineAgent()
-        test_agent(agent=agent, blackjack_env=env_2, num_turns=num_turns)
-    if test_dqn:
-        from agents.dqn import DQNAgent
-        agent = DQNAgent(env=env_1, epsilon=0, loaded_model="models/128x64x32_val=-0.035_1702857898.model")
-        test_agent(agent=agent, blackjack_env=env_2, num_turns=num_turns)
-    if all:
-        from agents.baseline import BaselineAgent
-        from agents.dqn import DQNAgent
-        from agents.random_agent import RandomAgent
-        baseline_agent = BaselineAgent()
-        dqn_agent = DQNAgent(env=env_1, epsilon=0, loaded_model="models/128x64x32_val=-0.035_1702857898.model")
-        random_agent = RandomAgent()
-        plays = []
-        num_plays = 0
-        baseline_data = {'rewards': [], 'avg_rewards': []}
-        dqn_data = {'rewards': [], 'avg_rewards': []}
-        random_data = {'rewards': [], 'avg_rewards': []}
-        plt.show()
-        for i in range(num_turns):
-            rew1 = end_play(agent=baseline_agent, env=env_1)
-            rew2 = end_play(agent=dqn_agent, env=env_2)
-            rew3 = end_play(agent=random_agent, env=env_3)
+        num_plays += 1
+        plays.append(num_plays)
 
-            num_plays += 1
-            plays.append(num_plays)
+        baseline_data['rewards'].append(rew1)
+        dqn_data['rewards'].append(rew2)
+        random_data['rewards'].append(rew3)
 
-            baseline_data['rewards'].append(rew1)
-            dqn_data['rewards'].append(rew2)
-            random_data['rewards'].append(rew3)
+        avg_reward1 = sum(baseline_data['rewards'])
+        avg_reward1 /= num_plays
+        baseline_data['avg_rewards'].append(avg_reward1)
 
-            avg_reward1 = sum(baseline_data['rewards'])
-            avg_reward1 /= num_plays
-            baseline_data['avg_rewards'].append(avg_reward1)
+        avg_reward2 = sum(dqn_data['rewards'])
+        avg_reward2 /= num_plays
+        dqn_data['avg_rewards'].append(avg_reward2)
 
-            avg_reward2 = sum(dqn_data['rewards'])
-            avg_reward2 /= num_plays
-            dqn_data['avg_rewards'].append(avg_reward2)
+        avg_reward3 = sum(random_data['rewards'])
+        avg_reward3 /= num_plays
+        random_data['avg_rewards'].append(avg_reward3)
 
-            avg_reward3 = sum(random_data['rewards'])
-            avg_reward3 /= num_plays
-            random_data['avg_rewards'].append(avg_reward3)
-
-            plt.subplot(311)
-            fig = plt.gcf()
-            for txt in fig.texts:
-                txt.set_visible(False)
-            
-            print_plot(baseline_data, avg_reward1, 311, 'Baseline Agent', 0.75, 'blue')
-            print_plot(dqn_data, avg_reward2, 312, 'DQN Agent', 0.5, 'red')
-            print_plot(random_data, avg_reward3, 313, 'Random Agent', 0.25, 'black')
-            
-            plt.pause(0.01) 
+        plt.subplot(311)
+        fig = plt.gcf()
+        for txt in fig.texts:
+            txt.set_visible(False)
+        
+        print_plot(baseline_data, avg_reward1, 311, 'Baseline Agent', 0.75, 'blue')
+        print_plot(dqn_data, avg_reward2, 312, 'DQN Agent', 0.5, 'red')
+        print_plot(random_data, avg_reward3, 313, 'Random Agent', 0.25, 'black')
+        
+        plt.pause(0.01) 
